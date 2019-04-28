da543c8b4df654f26c4e89200bb5acb49aaaeaed
Craig Sirota <34724663+csirota97@users.noreply.github.com>
2019-04-28 16:51:02 -0400

    Delete networking2.py

--- networking2.py
+++
@@ -1,6 +0,0 @@
-import socket
-# Principles Of Programming Languages - CS314
-# Final Project
-# File: networking2.py
-# Names: Craig Sirota and Dov Kassai
-my_ip = socket.gethostbyname(socket.gethostname())
